111d757577cd28e86b9d92e57d4be2205ad8cd48
Francis Arturo Alcalde <francisarturoalcaldesocole@gmail.com>
2023-11-30 13:59:53 -0300

    minor update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <div className='header'>
+    <header className='header'>
         <h1>Lista de tareas</h1>   
-    </div>
+    </header>
   )
 }
 
